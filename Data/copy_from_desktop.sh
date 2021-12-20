d2226dcee9212c2c9d2db92d031db3c864293253
kbidzhiev <bidzhiev.kemal@physics.msu.ru>
2021-12-20 11:40:04 +0100

    ..

---
+++ Data/copy_from_desktop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#scp -r kbidzhiev@entree.lptms.u-psud.fr:/home/kbidzhiev/Programs/Random_perturbation/Data/XXZ_NI/Rnd/1/*  /home/kemal/Programs/Random_perturbation/Data/XXZ_NI/Rnd/1/
+
+#scp -r kbidzhiev@entree.lptms.u-psud.fr:/home/kbidzhiev/Programs/Random_perturbation/Data/XXZ_NI/Rnd/2/*  /home/kemal/Programs/Random_perturbation/Data/XXZ_NI/Rnd/2/
+
+#scp -r kbidzhiev@entree.lptms.u-psud.fr:/home/kbidzhiev/Programs/Random_perturbation/Data/XXZ_NI/Rnd/no_perturbation/*  /home/kemal/Programs/Random_perturbation/Data/XXZ_NI/Rnd/no_perturbation/
+
+
+scp -r kbidzhiev@entree.lptms.u-psud.fr:/home/kbidzhiev/Programs/Random_perturbation/Data/XY_NI/UDD/1/*  /home/kemal/Programs/Random_perturbation/Data/XY_NI/UDD/1
+
+scp -r kbidzhiev@entree.lptms.u-psud.fr:/home/kbidzhiev/Programs/Random_perturbation/Data/XY_NI/UDD/2/*  /home/kemal/Programs/Random_perturbation/Data/XY_NI/UDD/2
+
+scp -r kbidzhiev@entree.lptms.u-psud.fr:/home/kbidzhiev/Programs/Random_perturbation/Data/XY_NI/UDD/no_perturbation/*  /home/kemal/Programs/Random_perturbation/Data/XY_NI/UDD/no_perturbation
